d9477b206391b8fe02ea2332484e6849d7028baa
MarinaChepchugova <chepchugova.m82@gmail.com>
2022-07-02 07:15:08 +1100

    spoiler

---
+++ js/style.js
@@ -0,0 +1,10 @@
+const headers = document.querySelectorAll("[data-name='spoiler-title']");
+
+headers.forEach(function (item) {
+  item.addEventListener("click", headerClick);
+});
+
+function headerClick() {
+	console.log(headerClick)
+  this.nextElementSibling.classList.toggle("spoiler-body");
+}
